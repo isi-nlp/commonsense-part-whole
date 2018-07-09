ecb2b06de34439cd52be6c13d1df52a359d93219
jamesmullenbach <james.mullenbach@gmail.com>
2018-07-09 11:57:12 -0700

    code to label and split sentence data

---
+++ src/dataproc/split_and_label_snli_style.py
@@ -0,0 +1,53 @@
+"""
+    Read snli style sentence data for each part-whole-adjective. 
+    Copy labels and splits from that that exists for triple datasets
+"""
+import argparse, csv, random
+import numpy as np
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', type=str, help='path to snli style sentences data')
+    parser.add_argument('outfile', type=str, help='path to write annotated data. base name: will append _train, _dev, _test')
+    args = parser.parse_args()
+
+    folds = ['train', 'dev', 'test']
+    #for fold, outwriter in zip(folds, [trw, dvw, tew]):
+    trip2labels = {} #(whole, part, adj): (split, label, bin_label)
+    for fold in folds:
+        with open('../../data/annotated/full_%s.csv' % fold) as f:
+            r = csv.reader(f)
+            #header
+            next(r)
+            for row in r:
+                tup = tuple(row[:3])
+                pw2labels[tup] = (fold, row[3], row[4])
+
+    trf = open(args.outfile.replace('.csv', '_train.csv'), 'w')
+    dvf = open(args.outfile.replace('.csv', '_dev.csv'), 'w')
+    tef = open(args.outfile.replace('.csv', '_test.csv'), 'w')
+
+    trw = csv.writer(trf, delimiter='\t')
+    trw.writerow(['whole', 'part', 'jj', 'hypothesis', 'context', 'label', 'bin_label'])
+    dvw = csv.writer(dvf, delimiter='\t')
+    dvw.writerow(['whole', 'part', 'jj', 'hypothesis', 'context', 'label', 'bin_label'])
+    tew = csv.writer(tef, delimiter='\t')
+    tew.writerow(['whole', 'part', 'jj', 'hypothesis', 'context', 'label', 'bin_label'])
+    
+    with open(args.file) as f:
+        r = csv.reader(f, delimiter='\t')
+        #header
+        next(r)
+        for row in r:
+            whole, part, jj = tuple(row[:3])
+            split, label, bin_label = trip2labels[(whole, part, jj)]
+            if split == 'train':
+                trw.writerow([whole, part, jj, row[4], row[5], label, bin_label])
+            if split == 'dev':
+                dvw.writerow([whole, part, jj, row[4], row[5], label, bin_label])
+            if split == 'test':
+                tew.writerow([whole, part, jj, row[4], row[5], label, bin_label])
+
+    trf.close()
+    dvf.close()
+    tef.close()
