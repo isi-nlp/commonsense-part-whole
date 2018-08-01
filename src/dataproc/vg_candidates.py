c034b1437ba0d8dc51b965821710418c89e975af
James Mullenbach <james.mullenbach@gmail.com>
2018-08-01 16:28:36 -0700

    add semeval 2010 task 8 data. default to 'late fusion' of bbox info. remove some old code

--- src/dataproc/vg_candidates.py
+++ src/dataproc/vg_candidates.py
@@ -25,86 +25,12 @@ class WhitespaceTokenizer(object):
         spaces = [True] * len(words)
         return Doc(self.vocab, words=words, spaces=spaces)
 
-def tfidf(whole, part, subj_obj_imgs):
-    """
-        Compute the tfidf-like ratio (# images with (whole, part)) / (# images with part *as the part in a has relation*)
-        If the ratio is too low, we will prune this (whole, part)
-    """
-    tf = len(subj_obj_imgs[(whole, part)])
-    df = sum([len(imgs) for ((_, part_), imgs) in subj_obj_imgs.items() if part_ == part])
-    return tf/df
-
-def tfidf_abs(whole, part, subj_obj_imgs, abs_noun_counts):
-    """
-        Compute the tfidf-like ratio (# images with (whole, part)) / (# images with part *anywhere*)
-        If the ratio is too low, we will prune this (whole, part)
-    """
-    tf = len(subj_obj_imgs[(whole, part)])
-    df = abs_noun_counts[part]
-    try:
-        return tf/df
-    except:
-        import pdb; pdb.set_trace()
-
 def get_name_and_id(rel, arg):
     if 'name' in rel[arg]:
         name = rel[arg]['name']
     elif 'names' in rel[arg]:
         name = rel[arg]['names'][0]
     return re.sub('\s\s+', ' ', name), rel[arg]['object_id']
-
-def get_part_to_filtered_lookup(vgrels):
-    ### spacy works much faster when you give it a lot of text at once
-    ### so, here's an initial loop to read all part nouns, pos-tag and a filter them,
-    ### and create a lookup. Also remove non-alphabetic chars
-    print("making part to filtered lookup")
-    print("filtering all part nouns")
-    parts = set()
-    for img in tqdm(vgrels):
-        for rel in img['relationships']:
-            if rel['predicate'] in ['has', 'has a', 'have', 'has an']:
-                parts.add(get_name_and_id(rel, 'object')[0])
-                parts.add(get_name_and_id(rel, 'subject')[0])
-    parts = list(parts)
-    inds = np.cumsum([len(p.split()) for p in parts])
-    words = re.sub('\s\s+', ' ', " ".join(parts))
-    print("pos tagging...")
-    doc = nlp(words)
-    part2filtered = {}
-    cur_orig_part = []
-    cur_filt = None
-    ix = 0
-    for i, t in enumerate(doc):
-        if i < inds[ix]:
-            cur_orig_part.append(t.text)
-            if t.pos_ == 'NOUN':
-                cur_filt = t.text
-        elif i == inds[ix]:
-            if cur_filt is not None:
-                part2filtered[' '.join(cur_orig_part)] = re.sub('[^a-zA-Z]', '', cur_filt)
-            ix += 1
-            cur_orig_part = [t.text]
-            cur_filt = t.text if t.pos_ == 'NOUN' else None
-    return parts, part2filtered
-
-def get_part_to_filtered_lookup_wordnet(vgrels):
-    print("making part to filtered lookup with wordnet")
-    print("filtering all part nouns")
-    parts = set()
-    for img in tqdm(vgrels):
-        for rel in img['relationships']:
-            if rel['predicate'] in ['has', 'has a', 'have', 'has an']:
-                obj = get_name_and_id(rel, 'object')[0]
-                subj = get_name_and_id(rel, 'subject')[0]
-                parts.add()
-                parts.add()
-    part2filtered = {}
-    for part in parts:
-        if ' ' in part:
-            if len(wn.synsets(part)) > 0:
-                part2filtered[part]
-        else:
-            part2filtered[part] = part
 
 if __name__ == "__main__":
     print("loading visual genome relationships...")
@@ -126,8 +52,6 @@ if __name__ == "__main__":
         #how many times each noun appears at all (not just in a 'has' relationship)
         abs_noun_counts = Counter()
 
-        #parts, part2filtered = get_part_to_filtered_lookup(vgrels)
-        
         print("extracting parts and wholes from 'has' relations...")
         for img in tqdm(vgrels):
             added = set()
@@ -138,12 +62,6 @@ if __name__ == "__main__":
                 subj = rel['subject']
                 subj_name, subj_id = get_name_and_id(rel, 'subject')
                 obj_name, obj_id = get_name_and_id(rel, 'object')
-
-                #filter names
-                #if obj_name in part2filtered:
-                #    obj_name = part2filtered[obj_name]
-                #if subj_name in part2filtered:
-                #    subj_name = part2filtered[subj_name]
 
                 if obj_name is None or subj_name is None:
                     continue
@@ -218,21 +136,11 @@ if __name__ == "__main__":
                     else:
                         obj_ss = None
 
-                    #subj_objs.add((subj, subj_ss, obj, obj_ss))
-                    #subj_objs.append((subj, subj_ss, obj, obj_ss))
                     subj_obj_counts[(subj_name, obj_name)] += 1
                     if (subj_name, obj_name) not in added:
                         bbox = BboxInfo(img['image_id'], obj['x'], obj['y'], obj['w'], obj['h'], subj['x'], subj['y'], subj['w'], subj['h'])
                         subj_obj_imgs[(subj_name, obj_name)].append(bbox)
                         added.add((subj_name, obj_name))
-
-        #print("calculating tfidfs...")
-        #tfidfs = defaultdict(lambda: {})
-        ##tfidfs_abs = defaultdict(lambda: {})
-        #for (whole, part) in tqdm(subj_obj_imgs.keys()):
-        #    tfidfs[whole][part] = tfidf(whole, part, subj_obj_imgs)
-        #    #tfidfs_abs[whole][part] = tfidf_abs(whole, part, subj_obj_imgs, abs_noun_counts)
-
 
         whole_imgs = defaultdict(set)
         part_imgs = defaultdict(set)
@@ -247,8 +155,3 @@ if __name__ == "__main__":
                 random.shuffle(imgs)
                 sample = [json.dumps(img._asdict()) for img in imgs[:3]]
                 w.writerow([whole, part, len(whole_imgs[whole]), len(part_imgs[part]), len(subj_obj_imgs[(whole, part)]), *sample])
-        #for whole, lookup in tfidfs.items():
-        #    for part, score in lookup.items():
-        #        #this set of requirements also implicitly requires a part to show up at least 3 times
-        #        if len(subj_obj_imgs[(whole, part)]) > 1 and score > 0.1 and score != 1.0 and score != 0.5 and part != whole:
-        #            w.writerow([whole, part, len(whole_imgs[whole]), len(part_imgs[part]), len(subj_obj_imgs[(whole, part)])])
